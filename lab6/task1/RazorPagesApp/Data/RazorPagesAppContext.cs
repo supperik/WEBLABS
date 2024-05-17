ca600044f42f2f3cc078a97bdc89453d3409188c
supperik <mr.niki234@mail.ru>
2024-05-18 00:55:10 +0300

    add lab6

---
+++ lab6/task1/RazorPagesApp/Data/RazorPagesAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesApp.Data
+{
+    public class RazorPagesAppContext : DbContext
+    {
+        public RazorPagesAppContext(DbContextOptions<RazorPagesAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ lab6/task1/RazorPagesApp/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [Display(Name = "Дата выпуска")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Rating { get; set; }
+    }
+}
